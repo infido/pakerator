36db0cf810243fd2919cbd3cbcfbdf073571bd16
Jacek Dziwoki <j.dziwoki@infido.pl>
2016-01-05 21:16:04 +0100

    Program z połaczeniem do bazy

---
+++ Pakerator/Pulpit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace Pakerator
+{
+    public partial class Pulpit : Form
+    {
+        public ConnectionDB polaczenie;
+        private FbDataAdapter fda;
+        
+        public Pulpit()
+        {
+            InitializeComponent();
+            Text = "Pakerator " + Application.ProductVersion;
+            polaczenie = new ConnectionDB();
+        }
+
+        private void Pulpit_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Pulpit_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            polaczenie.Close();
+        }
+    }
+}
