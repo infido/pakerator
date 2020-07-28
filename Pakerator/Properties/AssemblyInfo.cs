1b06b55d6c839517e590fdee62f9305449d86170
Jacek Dziwoki <jacek.dziwoki@gmail.com>
2020-07-28 13:30:54 +0200

    Wersja 2.1

--- Pakerator/Properties/AssemblyInfo.cs
+++ Pakerator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Pakerator")]
 [assembly: AssemblyDescription("System do obsługi kodów kreskowych w RaksSQL")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("www.INFIDO.pl")]
+[assembly: AssemblyCompany("Jacek Dziwoki")]
 [assembly: AssemblyProduct("Pakerator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.34")]
-[assembly: AssemblyFileVersion("1.34")]
+[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyFileVersion("2.1")]
